b2a84544ec935a2cfafe5fe7cb88f69cb4a17b52
developer-aj <ajju.kvmc@gmail.com>
2016-05-17 02:32:19 +0530

    using  service to fetch data

--- Angular/controllers.js
+++ Angular/controllers.js
@@ -1,19 +1,13 @@
-app.controller('ctrl', function($scope) {
-    $scope.customers = [
-		{name:'John Smith', city:'Phoenix'},
-		{name:'Adam Smith', city:'Phoenix'},
-		{name:'Cristiano Ronaldo', city:'Phoenix'},
-		{name:'Johny', city:'Phoenix'},
-		{name:'Emilia Clarke', city:'Meereen'}
-	];
+app.controller('ctrl', function($scope, $http) {
+ $http.get("http://www.w3schools.com/angular/customers.php").then(function (response){
+        $scope.customers = response.data.records;
+    }); 
 });
 
 app.run(function($rootScope, $location, $http, $timeout) {
     $rootScope.orderByX = "name";
     $rootScope.pageHeader = "You are currently browsing : " + $location.absUrl();
-    $http.post("http://webscraping.herokuapp.com/", "csrfmiddlewaretoken:'JqyvJkhrPftJ3yshSLgCf7LyuAulqIy4',website:'www.google.com'").then(function (response){
-        $rootScope.responseData = response.data;
-    });
+    
     $timeout(function(){
         $rootScope.pageHeader = "Hi there !";
     }, 5000);
--- Angular/directives.js
+++ Angular/directives.js
@@ -3,14 +3,17 @@ app.directive("newDirective", function() {
         restrict: "A",
         template: "<thead>" +
                         "<tr>" +
-                            "<th ng-click=\"changeOrderBy('name')\">Name</th>" +
-                            "<th ng-click=\"changeOrderBy('city')\">City</th>" +
+                            "<th ng-click=\"changeOrderBy('Name')\">Name</th>" +
+                            "<th ng-click=\"changeOrderBy('City')\">City</th>" +
+                            "<th ng-click=\"changeOrderBy('Country')\">Country</th>"
+                            +
                         "</tr>" +
                     "</thead>" +
                     "<tbody ng-controller=\"ctrl\">" +
-                          "<tr ng-repeat=\"cust in customers | filter:name | filter:city | orderBy:orderByX:false\">" +
-                            "<td ng-bind=\"cust.name\"></td>" +
-                            "<td ng-bind=\"cust.city\"></td>" +
+                          "<tr ng-repeat=\"cust in customers | filter:Name | filter:City | filter:Country | orderBy:orderByX:false\">" +
+                            "<td ng-bind=\"cust.Name\"></td>" +
+                            "<td ng-bind=\"cust.City\"></td>" +
+                            "<td ng-bind=\"cust.Country\"></td>" +
                           "</tr>" +
                     "</tbody>"
     };
