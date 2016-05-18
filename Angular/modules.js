0ee3af3899c0dc44204f4a3c8ff8b3003d88dfa5
developer-aj <ajju.kvmc@gmail.com>
2016-05-19 01:52:51 +0530

    commit

--- Angular/modules.js
+++ Angular/modules.js
@@ -1,1 +1,15 @@
 var app = angular.module('app', []);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'ctrl',
+                templateUrl: 'Partials/View1.html'
+            })
+        .when('/add', 
+            {
+                controller: 'ctrl',
+                templateUrl: 'Partials/View2.html'
+            })
+        .otherwise({ redirectTo: '/' });
+});
